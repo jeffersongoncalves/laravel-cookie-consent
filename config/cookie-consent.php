5e094d1b432b4dad5d7478fbe95fa8c34a4d9dd1
jeffersongoncalves <411493+jeffersongoncalves@users.noreply.github.com>
2025-03-01 21:10:10 +0000

    Fix styling

--- config/cookie-consent.php
+++ config/cookie-consent.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-    "content" => [
-        "message" => "This website uses cookies and other tracking technologies to assist with navigation, monitor site usage and web traffic, assist with our promotions and marketing, and personalize and improve our services and websites, as set forth in our",
-        "dismiss" => "Accept",
-        "link" => "Privacy Policy",
+    'content' => [
+        'message' => 'This website uses cookies and other tracking technologies to assist with navigation, monitor site usage and web traffic, assist with our promotions and marketing, and personalize and improve our services and websites, as set forth in our',
+        'dismiss' => 'Accept',
+        'link' => 'Privacy Policy',
         'href' => null,
     ],
-    "palette" => [
-        "popup" => [
-            "background" => "#696969",
-            "text" => "#FFFFFF",
+    'palette' => [
+        'popup' => [
+            'background' => '#696969',
+            'text' => '#FFFFFF',
         ],
-        "button" => [
-            "background" => "#FFAB00",
-            "text" => "#FFFFFF",
+        'button' => [
+            'background' => '#FFAB00',
+            'text' => '#FFFFFF',
         ],
     ],
     'position' => 'bottom-left',
